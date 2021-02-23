53845a11ebb4ffb36c1758719c6997ed2b7d190b
Juan Ceballos <juanceballos@Juan-Mac-Air.local>
2021-02-23 18:32:21 -0500

    Twelve-th commit

--- poll/models.py
+++ poll/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Option(models.Model):
+    answer = models.CharField(max_length=127)
+    coeficient = models.DecimalField(max_digits=5,decimal_places=5)
+    votes = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.answer} coeficiente {self.coeficient} votes {self.votes}"
+
 class Poll(models.Model):
     question = models.CharField(max_length=127)
-    winning_option = models.CharField(max_length=127)
-    winning_coeficient = models.DecimalField(max_digits=5,decimal_places=5)
-    horario_inicio = models.DateTimeField()
-    horario_fin = models.DateTimeField()
+    winning_option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.question}"
--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Option
+
 # Create your views here.
+def index(request):
+    return render(request, "poll/index.html", {
+        "options": Option.objects.all()
+    })
