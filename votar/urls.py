9876fded087f1bcebc5e116640e658839600fd69
cebajuab <juanceballos@mac.com>
2021-03-09 16:53:23 -0500

    21st commit

--- votar/urls.py
+++ votar/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.votar_view, name="votar"),
-    path("<int:poll_id>", views.vota_view, name="votar")
+    path("", views.index, name="votar"),
+    path("<int:poll_id>", views.voto_view, name="voto")
 ]
--- votar/views.py
+++ votar/views.py
@@ -3,17 +3,12 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+
 from poll.models import Poll, Option, Vote
 
 # Create your views here.
 def index(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
-    return render(request, "poll/votar.html", {
-            "polls": Poll.objects.all()
-        })
-
-def votar_view(request):
     if request.user.is_authenticated:
         return render(request, "poll/votar.html", {
             "polls": Poll.objects.all()
@@ -21,12 +16,19 @@ def votar_view(request):
     else:
         return HttpResponseRedirect(reverse("login"))
 
-def vota_view(request, poll_id):
+def voto_view(request, poll_id):
     if request.user.is_authenticated:
         return render(request, "poll/vota.html", {
             "poll": Poll.objects.get(pk=poll_id),
-            "options": Option.objects.filter(poll=poll_id),
-            "votes": Vote.objects.filter(poll=poll_id).order_by('option')
+            "options": Option.objects.filter(poll=poll_id)
         })
     else:
         return HttpResponseRedirect(reverse("login"))
+
+def votacion_view(request, poll_id):
+    if request.method == "POST":
+        option = Option.objects.get(pk=int(request.POST["option"]))
+        poll = Poll.objects.get(pk=poll_id)
+        voto = Vote(poll=poll, option=option, voter=request.user)
+        voto.save()
+        return HttpResponseRedirect(reverse("votar", args=(poll.id,)))
